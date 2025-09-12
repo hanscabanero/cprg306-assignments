b34e2ac803cd71992b26204b15e39013b0bfad8c
Hans Cabanero <hanscabanero@gmail.com>
2025-09-12 15:51:37 -0600

    initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+ 
+export default function Page()
+{
+  return (
+    <main>
+      <h1>This is my home page</h1>
+      <Link href="/week-2">Go to week 2 page</Link>
+ 
+    </main>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+export default function Page()
+{
+    return (
+        <main >
+            <h1>This is my home page</h1>
+            <p>You created week 2 route</p>
+        </main>
+    );
+ 
+}
