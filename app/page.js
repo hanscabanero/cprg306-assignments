8dc5a678d1543d748843bee6c593b549055095de
Hans Cabanero <hanscabanero@gmail.com>
2025-09-16 11:44:37 -0600

    changed Week 2 assignment link

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Page()
   return (
     <main>
       <h1>CPRG306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Go to week 2 page</Link>
+      <Link href="/week-2">Week 2 Assignment</Link>
     </main>
   );
 }
