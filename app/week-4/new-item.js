3d688157517bb002387a490aec73a40184b566f1
Hans Cabanero <hanscabanero@gmail.com>
2025-10-02 13:10:47 -0600

    added tailwind styling to week-3 page

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -16,14 +16,16 @@ export default function NewItem()
     }
 
   return (
-    <div className="flex items-center gap-4">
-      <button onClick={decrementQuantity} disabled={quantity == 1}>
-        -
-      </button>
-      <span>{quantity}</span>
-      <button onClick={incrementQuantity} disabled={quantity == 20}>
-        +
-      </button>
+    <div className="flex items-center justify-center gap-4 h-screen flex-col">
+      <div className="bg-gray-200 p-10 rounded-xl">
+          <button className="w-16 p-6 bg-blue-500 text-white text-2xl rounded-xl" onClick={decrementQuantity} disabled={quantity == 1}>
+            -
+          </button>
+          <span className="font-mono m-10 text-5xl align-middle">{quantity}</span>
+          <button className="p-6 bg-blue-500 text-white text-2xl rounded-xl" onClick={incrementQuantity} disabled={quantity == 20}>
+            +
+          </button>
+      </div>
     </div>
   );
 }
