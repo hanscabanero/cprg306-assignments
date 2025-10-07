b3d7665adb8f38dd9b46ee338f93355dc1b0b48c
Hans Cabanero <hanscabanero@gmail.com>
2025-10-07 13:39:59 -0600

    completed page functionality, and commented code

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -1,11 +1,5 @@
-"use client";
-import {useState} from "react";
-
-export default function NewItem() 
+export default function NewItem({quantity, setQuantity}) 
 {
-  const [quantity, setQuantity] = useState(1);
-
-
     function incrementQuantity() 
     {
         setQuantity(quantity + 1);
@@ -18,11 +12,11 @@ export default function NewItem()
   return (
     <div className="align-middle flex flex-row items-center">
       <div className="rounded-xl border p-2 border-gray-300">
-          <button className="px-4 py-2 bg-red-500 text-white text-2xl rounded-xl" type="button" onClick={decrementQuantity} disabled={quantity == 1}>
+          <button className="px-4 py-2 bg-red-500 text-white text-2xl rounded-xl disabled:bg-gray-500 " type="button" onClick={decrementQuantity} disabled={quantity == 1}>
             -
           </button>
           <span className="font-mono m-6 text-xl align-middle">{quantity}</span>
-          <button className="px-4 py-2 bg-green-500 text-white text-2xl rounded-xl" type="button" onClick={incrementQuantity} disabled={quantity == 20}>
+          <button className="px-4 py-2 bg-green-500 text-white text-2xl rounded-xl disabled:bg-gray-500" type="button" onClick={incrementQuantity} disabled={quantity == 20}>
             +
           </button>
       </div>
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,17 +1,56 @@
+"use client";
+import { useState } from "react";
 import NewItem from "./new-item.js";
 
-export default function Page()
-{
+export default function Page() {
+    // declare state variables for the form inputs
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("produce");
+    const [quantity, setQuantity] = useState(1);
+
+
+    function handleSubmit(e) {
+        e.preventDefault(); // Stop from reloading the page
+        alert(`Name: ${name}
+            \nQuantity: ${quantity}
+            \nCategory: ${category}`);
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+    }
+
     return (
         <main>
-            <form className="mx-auto my-4 gap-3 flex flex-col justify-center text-black bg-white w-100 p-4 border border-gray-300 rounded-lg shadow-2xl">
+            <form
+                className="mx-auto my-4 gap-3 flex flex-col justify-center text-black bg-white w-100 p-4 border border-gray-300 rounded-lg shadow-2xl"
+                // call submit handler function
+                onSubmit={handleSubmit}
+            >
                 <h1 className="mx-auto text-xl font-bolder mb-4">
                     Week-5 Assignment
                 </h1>
-                <input placeholder="Item Name" className="p-2 border border-gray-300 rounded" type="text" name="name" id="name" />
+                <input
+                    placeholder="Item Name"
+                    className="p-2 border border-gray-300 rounded"
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    // set the name state variable when the user types in the input field
+                    onChange={e => setName(e.target.value)}
+                />
                 <div className="flex flex-row justify-between gap-4">
-                    <NewItem/>
-                    <select className="p-2 border border-gray-300 rounded" name="category" id="category">
+                    {/* quantity selector, pass quantity into new-item component*/}
+                    <NewItem quantity={quantity} setQuantity={setQuantity} />
+                    <select
+                        className="p-2 border border-gray-300 rounded"
+                        name="category"
+                        id="category"
+                        value={category}
+                        // set the category state variable when the user selects a different option
+                        onChange={e => setCategory(e.target.value)}
+                    >
+                        {/* provide values for user to choose and store them */}
                         <option value="produce">Produce</option>
                         <option value="dairy">Dairy</option>
                         <option value="bakery">Bakery</option>
@@ -25,9 +64,9 @@ export default function Page()
                         <option value="Others">Others</option>
                     </select>
                 </div>
+                {/* submit button */}
                 <button className="bg-blue-500 text-white p-3 rounded-lg" type="submit"> Add Item </button>
             </form>
         </main>
     );
- 
 }
