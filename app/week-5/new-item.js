d0b1fa44b4c2657a8080b4d3c65cd2ef3672a926
koreanjesus101 <sean.lee@edu.sait.ca>
2025-10-07 15:32:31 -0600

    added functions styling
    added functions for buttons and styled box to be center with plus and minus buttons grey if hit setQuantity

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -18,11 +18,11 @@ export default function NewItem()
   return (
     <div className="align-middle flex flex-row items-center">
       <div className="rounded-xl border p-2 border-gray-300">
-          <button className="px-4 py-2 bg-red-500 text-white text-2xl rounded-xl" type="button" onClick={decrementQuantity} disabled={quantity == 1}>
+          <button className="px-4 py-2 bg-red-500 text-white text-2xl rounded-xl disabled:bg-gray-700" type="button" onClick={decrementQuantity} disabled={quantity == 1}>
             -
           </button>
           <span className="font-mono m-6 text-xl align-middle">{quantity}</span>
-          <button className="px-4 py-2 bg-green-500 text-white text-2xl rounded-xl" type="button" onClick={incrementQuantity} disabled={quantity == 20}>
+          <button className="px-4 py-2 bg-green-500 text-white text-2xl rounded-xl disabled:bg-gray-700" type="button" onClick={incrementQuantity} disabled={quantity == 20}>
             +
           </button>
       </div>
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,17 +1,49 @@
+"use client";
+import { useState } from "react";
 import NewItem from "./new-item.js";
 
-export default function Page()
-{
+export default function Page() {
+
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("produce");
+    const [quantity, setQuantity] = useState(1);
+
+
+    function handleSubmit(e) {
+        e.preventDefault(); 
+        alert(`Name: ${name} Category: ${category} Quantity: ${quantity}`);
+        setName("");
+        setCategory("produce");
+        setQuantity(1);
+    }
+
     return (
         <main>
-            <form className="mx-auto my-4 gap-3 flex flex-col justify-center text-black bg-white w-100 p-4 border border-gray-300 rounded-lg shadow-2xl">
+            <form
+                className="mx-auto my-4 gap-3 flex flex-col justify-center text-black bg-white w-100 p-4 border border-gray-300 rounded-lg shadow-2xl"
+                onSubmit={handleSubmit}
+            >
                 <h1 className="mx-auto text-xl font-bolder mb-4">
                     Week-5 Assignment
                 </h1>
-                <input placeholder="Item Name" className="p-2 border border-gray-300 rounded" type="text" name="name" id="name" />
+                <input
+                    placeholder="Item Name"
+                    className="p-2 border border-gray-300 rounded"
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
                 <div className="flex flex-row justify-between gap-4">
-                    <NewItem/>
-                    <select className="p-2 border border-gray-300 rounded" name="category" id="category">
+                    <NewItem quantity={quantity} setQuantity={setQuantity} />
+                    <select
+                        className="p-2 border border-gray-300 rounded"
+                        name="category"
+                        id="category"
+                        value={category}
+                        onChange={e => setCategory(e.target.value)}
+                    >
                         <option value="produce">Produce</option>
                         <option value="dairy">Dairy</option>
                         <option value="bakery">Bakery</option>
@@ -29,5 +61,4 @@ export default function Page()
             </form>
         </main>
     );
- 
 }
