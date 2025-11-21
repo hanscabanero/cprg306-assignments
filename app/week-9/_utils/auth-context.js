1fc5c1db956553e9251e24694d21b4aba19c17d7
Hans Cabanero <35246792+hanscabanero@users.noreply.github.com>
2025-11-21 16:24:31 -0700

    Refactor AuthContext useEffect dependency
    Updated useEffect dependency to ensure it runs only once.

--- app/week-9/_utils/auth-context.js
+++ app/week-9/_utils/auth-context.js
@@ -28,7 +28,7 @@ export const AuthContextProvider = ({ children }) => {
       setUser(currentUser);
     });
     return () => unsubscribe();
-  }, [user]);
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
@@ -40,3 +40,4 @@ export const AuthContextProvider = ({ children }) => {
 export const useUserAuth = () => {
   return useContext(AuthContext);
 };
+
